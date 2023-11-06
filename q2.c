8de41717d3b6302d40c96e9641f9ec7c6a146b7b
Mateus Amorim <142228584+knalen25@users.noreply.github.com>
2023-11-05 22:46:17 -0300

    q2.c

--- q2.c
+++ q2.c
@@ -1,120 +1,90 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
+    char time1[30], time2[30];
     char overT1[11][2][30];
     char overT2[11][2][30];
-    char time1[30], time2[30];
-    int peso;
+
     char* token;
-    float media; 
-    float valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10, valor11;
+
+    float media1 = 0, media2 = 0;
+    float valor1 = 0, valor2 = 0, valor5 = 0, valor6 = 0, valor8 = 0, valor10 = 0;
+    float v1 = 0, v2 = 0, v5 = 0, v6 = 0, v8 = 0, v10 = 0;
 
     fgets(time1, 30, stdin);
-        for (int i = 0; i < 11; i++)
-        {   
-            fgets(overT1, 30, stdin);
-            token = strtok(overT1, ";");
-            for (int j = 0; j < 2; j++)
-            {
-                token = strtok(NULL, ";");
-                printf(" %s", token);
-                strcpy(overT1[i][j], token);
-            }
-
+    time1[strcspn(time1, "\n")] = 0;
+    for (int i = 0; i < 11; i++) {
+        fgets(overT1[i][0], 30, stdin);
+        token = strtok(overT1[i][0], ";");
+        for (int j = 0; j < 2; j++) {
+            token = strtok(NULL, ";");
+            //printf(" %s", token);
+            strcpy(overT1[i][j], token);
         }
 
-        for (int i = 0; i < 11; i++)
-        {
-                if (strcmp(overT1[i][0], "G") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor1 = 8*atoi(overT1[i][1]);
-                }
-                
-                if (strcmp(overT1[i][0], "L") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor2 = 10*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "Z") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor3 = 5*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "Z") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor4 = 5*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "L") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor5 = 10*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "V") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor6 = 8*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "V") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor7 = 8*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "M") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor8 = 11*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "M") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor9 = 11*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "A") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor10 = 12*atoi(overT1[i][1]);
-                }
-
-                if (strcmp(overT1[i][0], "A") == 0)  //overT1[i][0] == G
-                {
-                    
-                    valor11 = 12*atoi(overT1[i][1]);
-                }
-
+        if (strcmp(overT1[i][0], "G") == 0) {
+            valor1 = 8 * atoi(overT1[i][1]);
+        } else if (strcmp(overT1[i][0], "L") == 0) {
+            valor2 += 10 * atoi(overT1[i][1]);
+        }else if (strcmp(overT1[i][0], "Z") == 0) {
+            valor6 += 5 * atoi(overT1[i][1]); 
+        }else if (strcmp(overT1[i][0], "V") == 0) {
+            valor5 += 8 * atoi(overT1[i][1]);
+        } else if (strcmp(overT1[i][0], "M") == 0) {
+            valor8 += 11 * atoi(overT1[i][1]);
+        } else if (strcmp(overT1[i][0], "A") == 0) {
+            valor10 += 12 * atoi(overT1[i][1]);
         }
-         
-        media = (valor1 + valor2 + valor3 + valor4 + valor5 + valor6 + valor7 + valor8 + valor9 + valor10 + valor11)/100; 
-        printf("%.2f", media);
-        
+    }
+
+    media1 = (valor1 + valor2 + valor5 + valor6 + valor8 + valor10) / 100;
+    //printf("%.2f\n", media1);
+
+    fgets(time2, 30, stdin);
+    time2[strcspn(time2, "\n")] = 0;
+    for (int i = 0; i < 11; i++) {
+        fgets(overT2[i][0], 30, stdin);
+        token = strtok(overT2[i][0], ";");
+        for (int j = 0; j < 2; j++) {
+            token = strtok(NULL, ";");
+            //printf(" %s", token);
+            strcpy(overT2[i][j], token);
+        }
+
+        if (strcmp(overT2[i][0], "G") == 0) {
+            v1 += 8 * atoi(overT2[i][1]);
+        } else if (strcmp(overT2[i][0], "L") == 0) {
+            v2 += 10 * atoi(overT2[i][1]);
+        } else if (strcmp(overT2[i][0], "Z") == 0) {
+            v6 += 5 * atoi(overT2[i][1]);
+        }else if (strcmp(overT2[i][0], "V") == 0) {
+            v5 += 8 * atoi(overT2[i][1]);
+        } else if (strcmp(overT2[i][0], "M") == 0) {
+            v8 += 11 * atoi(overT2[i][1]);
+        } else if (strcmp(overT2[i][0], "A") == 0) {
+            v10 += 12 * atoi(overT2[i][1]);
+        }
+    }
+
+    media2 = (v1 + v2 + v5 + v6 + v8 + v10) / 100;
+    //printf("%.2f\n", media2);
+
+
+    printf("%s: %.2f de forca\n", time1, media1);
+    printf("%s: %.2f de forca\n", time2, media2);
+
+    if (media1>media2)
+    {
+        printf("%s eh mais forte\n", time1);
+    }
+    else
+    {
+        printf("%s eh mais forte\n", time2);
+    }
     
-    /*fgets(time2, 30, stdin);
-        
-        for (int i = 0; i < 11; i++)
-        {   
-            fgets(overT2, 30, stdin);
-            token = strtok(overT2, ";");
-            for (int j = 0; j < 2; j++)
-            {
-                token = strtok(NULL, ";");
-                printf(" %s", token);
-                strcpy(overT2[i][j], token);
-            }
+    
 
-        }*/
-
-        
-
-
-
+    return 0;
 }
