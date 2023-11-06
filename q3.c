e81a40496d39d84076473ad193778143a2550f9d
Mateus Amorim <142228584+knalen25@users.noreply.github.com>
2023-11-06 09:02:59 -0300

    Update q3.c

--- q3.c
+++ q3.c
@@ -2,116 +2,49 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
     int m1[4][4], m2[4][4], resultado[4][4];
     char op[10];
-    
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             resultado[i][j] = 0;
         }
-        
     }
-    
 
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             scanf("%d", &m1[i][j]);
-            //printf("%d", m1[i][j]);
         }
-        
     }
-       
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             scanf("%d", &m2[i][j]);
-            //printf("%d", m1[x][y]);
         }
-        
     }
-    
+
     scanf("%s", op);
-    //printf("%s", op);
 
-    if (strcmp(op, "soma") == 0)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (strcmp(op, "soma") == 0) {
                 resultado[i][j] = m1[i][j] + m2[i][j];
-                printf("%d ", resultado[i][j]);
-                if (j == 3)
-                {
-                    printf("\n"); 
-                }
-                
-            }
-        }
-           
-    }
-
-    else if (strcmp(op, "sub") == 0)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
+            } else if (strcmp(op, "sub") == 0) {
                 resultado[i][j] = m1[i][j] - m2[i][j];
-                printf("%d ", resultado[i][j]);
-                if (j == 3)
-                {
-                    printf("\n");
-                }
-                
-            }
-            
-        }
-        
-    }
-
-    else if (strcmp(op, "mult") == 0)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                for (int k = 0; k < 4; k++)
-                {
+            } else if (strcmp(op, "mult") == 0) {
+                for (int k = 0; k < 4; k++) {
                     resultado[i][j] += m1[i][k] * m2[k][j];
                 }
-                
             }
-            
-        }
-        
-
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
+            printf("%4d", resultado[i][j]);
+            if (j==3)
             {
-                printf("%d ", resultado[i][j]);
-                if (j == 3)
-                {
-                    printf("\n");
-                }
-                
+                printf("\n");
             }
             
         }
-        
     }
-    
-    
-        
-    
 
-
-    return 0;   
+    return 0;
 }
